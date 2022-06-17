bdca90a4d5f7a38dc60d310645712024ae5f7b4a
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-06-18 04:47:17 +0700

    update: form register and validation

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    // Validators.pattern('[a-zA-Z ]*')
+  ])
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ])
+  age = new FormControl('', [
+    Validators.required,
+    Validators.max(120),
+    Validators.min(18),
+
+  ])
+  password = new FormControl('')
+  confirm_password = new FormControl('')
+  phoneNumber = new FormControl('')
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  })
+
+}
