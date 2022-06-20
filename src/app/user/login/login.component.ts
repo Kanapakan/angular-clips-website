7676a9d0fbc4c57bba208bd95d2a2b3d2c4c1f72
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-06-21 05:04:19 +0700

    feat: login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  credentials = {
+    email: '',
+    password: '',
+  }
 
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being create.'
+  alertColor = 'blue'
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.showAlert = true
+    this.alertMsg = 'login success'
+    this.alertColor = 'blue'
   }
 
 }
