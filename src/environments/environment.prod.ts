3b8ca489417eda9b8c59180ef2a1cd73bb4ecdf5
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-06-24 06:55:05 +0700

    feat: store users data in firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC8OtmHCTA9rcEyZnM2xug0aD2uPXtkpiw",
+    authDomain: "clips-e48f3.firebaseapp.com",
+    projectId: "clips-e48f3",
+    storageBucket: "clips-e48f3.appspot.com",
+    messagingSenderId: "52695758909",
+    appId: "1:52695758909:web:d1a30fb6e2b56afa1ed254"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC8OtmHCTA9rcEyZnM2xug0aD2uPXtkpiw",
+    authDomain: "clips-e48f3.firebaseapp.com",
+    projectId: "clips-e48f3",
+    storageBucket: "clips-e48f3.appspot.com",
+    messagingSenderId: "52695758909",
+    appId: "1:52695758909:web:d1a30fb6e2b56afa1ed254"
+  }
 };
 
 /*
