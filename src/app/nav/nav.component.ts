5d8ca7a528c7de27f62279a546821e739272fb3f
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-06-30 04:31:33 +0700

    feat: add manage route
    update: logout and redirect to home page when the route is authenticate only

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { ModalService } from 'src/app/services/modal.service';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-nav',
@@ -13,9 +13,7 @@ export class NavComponent implements OnInit {
   constructor(
     public modal: ModalService,
     public auth: AuthService,
-    private afAuth: AngularFireAuth
-    ) {
-     }
+    ) {}
 
   ngOnInit(): void {
   }
@@ -26,9 +24,4 @@ export class NavComponent implements OnInit {
     this.modal.toggleModal('auth')
   }
 
-  async logout($event: Event) {
-    $event.preventDefault()
-
-    await this.afAuth.signOut()
-  }
 }
